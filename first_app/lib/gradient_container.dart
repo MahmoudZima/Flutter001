e6317fe0dfdf1dfb877fa72a28b0ebf0639d5795
Mahmoud Omar <mahmoud.abdulazim@outlook.com>
2025-11-26 07:56:35 +0200

    Add Style Text and Stop in Variable

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -15,12 +16,7 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: const Center(
-        child: Text(
-          'Welcome to Gradient Container',
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-      ),
+      child: const Center(child: StyledText()),
     );
   }
 }
